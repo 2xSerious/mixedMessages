6c78b5f4f8c9d09437603dab738e9738bfae8bff
2xSerious <79917801+2xSerious@users.noreply.github.com>
2021-03-03 10:53:15 +0000

    First commit

---
+++ script.js
@@ -0,0 +1,60 @@
+// Mixed Messages Project 
+
+// Get random number 1-10
+function getRandomInt() {
+    return Math.ceil(Math.random() * 10)
+}
+// Objects 
+
+const breakfast = {
+    1: {
+        name: 'Protein packed oatmeal',
+        ingredients: '130g old rolled oats, 900ml water, 4 eggs, 2 banana, 480ml unsweetened plant-based milk, 1tsp cinnamon'
+        kcal: 298
+    },
+    2: {
+        name: 'Sweet Banana Omelette',
+        ingredients: '2 large eggs, 1 medium banana, 1tsp almond butter, 2tsp maple syrup, 4 raw walnut halves, 1Tbls dried coconut flakes, cinnamon, pinch of sea salt'
+        kcal: 441
+    },
+    3: {
+        name: 'Protein breakfast bowl',
+        ingredients: '4 eggs, 280g smoked ham, 400g cottage cheese, 2 apples',
+        kcal: 284
+    },
+    4: {
+        name: 'Chocolate chip banana bread pancakes',
+        ingredients: '180g all purpose flour, 2tsp baking powder, 2tsp cinnamon, 1/2tsp salt, 120ml unsweetened plant-based milk, 2 ripe banana, 2 eggs, 2tbsp canola oil, 120ml honey, 80g chopped dark chocolate',
+        kcal: 535
+    },
+    5: {
+        name: 'Mango pancakes',
+        ingredients: '300g mango, 6 eggs, 70g vanilla casein protein powder, 1/2tsp salt, 2tbsp coconut oil, 280g mixed berries',
+        kcal: 318
+    },
+    6: {
+        name: 'Apple pie oatmeal',
+        ingredients: '240g rolled oats, 960ml water, 1tsp salt, 4 apples, 1tbsp coconut oil, 1tsp cinnamon, 1tsp cardamom, 3tbsp honey, 140g vanilla flovered casein',
+        kcal: 456
+    },
+    7: {name: 'Cherrie and banana smoothie',
+        ingredients: '280ml of pitted frozen cherries, 480ml of Greek yogurt, 2 bananas, 4tbsp honey',
+        kcal: 273
+        
+    },
+    8: {
+        name: 'Salty breakfast bars',
+        ingredients: '140g almonds, 140g cashews, 140g susame seeds, 120ml egg whites, 1tsp chili flakes, 1 1/2tsp sea salt, 1tbsp lime zest',
+        kcal: 303
+    },
+    9: {
+        name: 'Toast with scramled tofu',
+        ingredients: '800g firm tofu, 3tbsp canola oil, 1tbsp turmeric, 4tbsp nuritional yeast, 4 slices rye bread, 1tsp salt, 1tsp ground balck pepper',
+        kcal: 302 
+    },
+    10: {
+        name: 'Bacon & egg breakfast tacos',
+        ingredients: '8 thin bacon slices, 8 eggs, Spray oil, 8 small tortillas, 30g of shredded cheese, 40g baby spinach, 1/2 small onion, 1 tomato, 2tsp sriracha sauce',
+        kcal: 345
+    }
+}
